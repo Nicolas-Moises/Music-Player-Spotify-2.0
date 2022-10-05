078e05228b18d40fcf54f4986f6c3bcee487c460
Nicolas Moises <nicolas.nicc21@gmail.com>
2022-10-05 01:08:24 -0300

    building component Discover

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,10 +2,28 @@ import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
 
 const Discover = () => {
-  console.log(genres);
+  const genreTitle = "Pop";
   return (
     <div className="flex flex-col ">
-      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10"></div>
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-sora font-bold text-2xl text-white text-left">
+          Explorar {genreTitle}
+        </h2>
+        <select
+          name=""
+          id=""
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div></div>
     </div>
   );
 };
